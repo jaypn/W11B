4735830c6155868ffbcf6ec15d80abe88941c86d
nyabuganda <jayp@nyabugandas-MacBook-Pro.local>
2020-09-09 20:18:20 -0600

    changed errors

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function getTrump(){
     ajax.onreadystatechange = function(){
         if (this.readyState == 4 && this.status == 200){
            let trumpQuote = JSON.parse(this.responseText);
-           document.getElementById("trumpquotes").innerHTML = trumpQuote.quote;
+           document.getElementById("trumpquotes").innerHTML = trumpQuote.joke;
     
         } else if(this.readyState != 4){
             document.getElementById("trumpquotes").innerHTML = "LOADING";
@@ -12,7 +12,7 @@ function getTrump(){
             document.getElementById("trumpquotes").innerHTML = "something went wrong"
         }
     }
-    ajax.open("GET", "https://healthruwords.p.rapidapi.com/v1/quotes/", true);
+    ajax.open("GET", "https://geek-jokes.sameerkumar.website/api?format=json", true);
     ajax.send();
     }
     
